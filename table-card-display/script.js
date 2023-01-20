83b432df3fed5e1a35a38ee812dd4c9e4cc98b88
Sherealyn <sherealyn@sentient.io>
2023-01-20 16:10:19 +0800

    table card switch view completed

--- table-card-display/script.js
+++ table-card-display/script.js
@@ -1,152 +1,112 @@
-console.log('script.js loaded')
+//console.log('script.js loaded')
 
-const users = []
+// Store userInfo into this array
+const users = [];
 
-const submitUser = () => {
-  console.log('Submit user')
+// Add data to the table
+function submitUser() {
+	//console.log('Submit user')
 
-  /**
-   * Prevent the default submission event refreshing the page.
-   */
-  document
-    .getElementById('userInputs')
-    .addEventListener('submit', function (event) {
-      event.preventDefault()
-    })
+	/**
+	 * Prevent the default submission event refreshing the page.
+	 */
+	document.getElementById('userInputs').addEventListener('submit', function (event) {
+		event.preventDefault();
+		
+	});
 
-  /**
-   * Get user info, from user input
-   */
-  const userInput = document.getElementById('userInputs')
 
-  const userInfo = {
-    name: userInput.name.value,
-    age: userInput.age.value,
-    id: users.length, // auto generated id based on the users list
-  }
+	/**
+	 * Get user info, from user input
+	 */
+	const userInput = document.getElementById('userInputs');
 
-  /**
-   * Store user, add the user in to users array
-   */
-  users.push(userInfo)
+	const userInfo = {
+		fname: userInput.fname.value,
+		country: userInput.country.value,
+		number: userInput.number.value,
+	};
 
-  /**
-   * Make sure the user been added to the users array
-   */
-  // console.log(users)
+	/**
+	 * Store user, add the user in to users array
+	 */
+	users.push(userInfo);
 
-  /**
-   * Adding the table row
-   */
-  // Select the table element
-  const tableElement = document.getElementById('userTable')
+	/**
+	 * Make sure the user been added to the users array
+	 */
+	// console.log(users)
 
-  // Create the table row
-  /**
-   * we want to re-build this chunk of html code
-   *    <tr>
-   *       <td>user name</td>
-   *       <td>user age</td>
-   *       <td>user id</td>
-   *     </tr>
-   */
-  const tableRow = document.createElement('tr')
+	// Get the table body
+	const tableBody = document.getElementById('tableBody');
 
-  // Build the td elements
-  const nameTd = document.createElement('td')
-  const ageTd = document.createElement('td')
-  const idTd = document.createElement('td')
+	// Select the table element
+	const tableElem = document.getElementById('userTable');
 
-  /**
-   * Fill in the value to Td elements
-   * with userInfo data
-   */
-  nameTd.innerText = userInfo.name
-  ageTd.innerText = userInfo.age
-  idTd.innerText = userInfo.id
+	// Create the table row
+	/**
+	 * we want to re-build this chunk of html code
+	 *    <tr>
+	 *       <td>user name</td>
+	 *       <td>user age</td>
+	 *       <td>user id</td>
+	 *     </tr>
+	 */
+	const tableRow = document.createElement('tr');
 
-  // Add the tds in to the tr
-  tableRow.append(nameTd, ageTd, idTd)
+	// Build the td elements
+	const fnameTd = document.createElement('td');
+	const countryTd = document.createElement('td');
+	const numberTd = document.createElement('td');
 
-  // Add the table row
-  tableElement.append(tableRow)
+	/**
+	 * Fill in the value to Td elements
+	 * with userInfo data
+	 */
+	fnameTd.innerText = userInfo.fname;
+	countryTd.innerText = userInfo.country;
+	numberTd.innerText = userInfo.number;
 
-  // Clean the user input fields
-  userInput.name.value = ''
-  userInput.age.value = ''
+	// Add the tds in to the tr
+	tableRow.append(fnameTd, countryTd, numberTd);
+
+	// Add the table row
+	tableElem.append(tableRow);
+
+	// Clean the user input fields
+	userInput.fname.value = '';
+	userInput.country.value = '';
+	userInput.number.value = '';
+
+	/**
+	 * Add data to card view
+	 */
+	const cardContainer = document.getElementById('cardView');
+
+	// Create div for card items
+	const cardItem = document.createElement('div');
+	cardItem.classList.add('cardItem');
+	cardItem.innerHTML = '<b>Full Name:</b> ' + userInfo.fname + '<br>' + '<b>Country:</b> ' + userInfo.country + '<br>' + '<b>Number:</b> ' + userInfo.number;
+	cardContainer.append(cardItem);
 }
 
-const switchCardView = () => {
-  // console.log('switch view')
+/**
+ * Function to switch between table and grid views
+ */
 
-  // Select the userCards element
-  const userCardsElem = document.getElementById('userCards')
+function switchView(view) {
+	// Get table and card elements
+	const tableView = document.getElementById('userTable');
+	const cardView = document.getElementById('cardView');
 
-  // Clean the userCards Element
-  userCardsElem.innerHTML = ''
-
-  /**
-   * Clear the table
-   */
-  document.getElementById('userTable').innerHTML = ''
-
-  /**
-   * Build the card with this structure
-   *   <div> // card container
-   * 
-        <div> // row container
-          <b>Name: </b> // title
-          User name  // variable
-        </div>
-
-        <div>
-          <b>Age: </b>
-          User age
-        </div>
-
-        <div>
-          <b>Id: </b>
-          User id
-        </div>
-      </div>
-   */
-
-  users.forEach((userInfo) => {
-    // Create card container
-    const cardContainer = document.createElement('div')
-
-    // Advanced way
-    /**
-     * const userInfo = {
-        name: userInput.name.value,
-        age: userInput.age.value,
-        id: users.length, // auto generated id based on the users list
-  }
-     */
-    // Object.keys(userInfo).forEach((key) => {
-    //   const rowContainer = document.createElement('div')
-    //   rowContainer.innerHTML = `<b>${key.charAt(0).toUpperCase() + key.slice(1)}: </b>` + userInfo[key]
-    //   cardContainer.append(rowContainer)
-    // })
-
-    // Create row container
-    const nameRowContainer = document.createElement('div')
-    nameRowContainer.innerHTML = '<b>Name: </b>' + userInfo.name
-    cardContainer.append(nameRowContainer)
-
-    const ageRowContainer = document.createElement('div')
-    ageRowContainer.innerHTML = '<b>Age: </b>' + userInfo.age
-    cardContainer.append(ageRowContainer)
-
-    const idRowContainer = document.createElement('div')
-    idRowContainer.innerHTML = '<b>Id: </b>' + userInfo.id
-    cardContainer.append(idRowContainer)
-
-    // Attach (append) row to card
- 
-
-    // Append the cardContainer
-    // Display the card by attach the card to userCards element
-    userCardsElem.append(cardContainer)
-  })
+	// Switch for table and card
+	if (view === 'table') {
+		tableView.style.display = 'table';
+		cardView.style.display = 'none';
+	} 
+	//For some reason, it has to be grid, otherwise it won't work.
+	else if (view === 'grid') {
+		tableView.style.display = 'none';
+		cardView.style.display = 'grid';
+	}
 }
